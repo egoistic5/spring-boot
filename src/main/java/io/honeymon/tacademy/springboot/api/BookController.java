fb79b7a303bd7d71e38e58fd2342e50010312384
민선 <minsun@minseon-ui-MacBookPro.local>
2020-03-22 00:36:53 +0900

    first commit

--- src/main/java/io/honeymon/tacademy/springboot/api/BookController.java
+++ src/main/java/io/honeymon/tacademy/springboot/api/BookController.java
@@ -5,18 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable; 
 
 import io.honeymon.tacademy.springboot.domain.Book;
 import io.honeymon.tacademy.springboot.service.BookService;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping(value="/books")
 public class BookController {
 	@Autowired
 	BookService bookService;
 	
 	@GetMapping("/{bookId")
-	public ResponseEntity<Book> findById(Long bookId) {
+	public ResponseEntity findById(@PathVariable Long bookId) {
 		Book book = bookService.findById(bookId)
 				.orElseThrow(() -> new RuntimeException("Not found : " + bookId));
 		return ResponseEntity.ok(book);
